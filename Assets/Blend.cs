9bd6457bca07d7f9dfeeb247a4db7e20f7acd0f5
Bar Ronen <barzie02@gmail.com>
2020-12-22 19:30:08 +0200

    Blending FAIL

---
+++ Assets/Blend.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blend : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    private void OnMouseDown()
+    {
+        spawnPoint.Rotate(0f, 180f, 0f);
+        Debug.Log("BLENDDDDDD");
+    }
+}
--- Assets/Scripts/JuiceSpawner.cs
+++ Assets/Scripts/JuiceSpawner.cs
@@ -10,10 +10,17 @@ public class JuiceSpawner : MonoBehaviour
 
     void OnMouseDown()
     {
+        StartCoroutine(WaitBetweenCubes());
+    }
+
+
+    IEnumerator WaitBetweenCubes()
+    {
         for (int i = 0; i < 20; i++)
         {
             var instance = Instantiate(fluidCube, juiceSpawnPoint);
             instance.transform.localPosition = Vector3.zero;
+            yield return new WaitForSeconds(0.2f);
         }
     }
 
