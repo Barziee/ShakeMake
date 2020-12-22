14587c5e62b76fa9842c0b331212b8bd2cb9d4d9
Bar Ronen <barzie02@gmail.com>
2020-12-22 18:41:43 +0200

    A ton of shit

---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+
+    public Transform spawnPoint;
+    
+
+    void OnMouseDown()
+    {
+        var instance = Instantiate(this.gameObject, spawnPoint);
+        instance.transform.localPosition = Vector3.zero;
+
+    }
+
+
+}
---
+++ Assets/Scripts/JuiceSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuiceSpawner : MonoBehaviour
+{
+    public Transform juiceSpawnPoint;
+    public GameObject fluidCube;
+
+
+    void OnMouseDown()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            var instance = Instantiate(fluidCube, juiceSpawnPoint);
+            instance.transform.localPosition = Vector3.zero;
+        }
+    }
+
+}
