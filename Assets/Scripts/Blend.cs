e0dce5b47cc020df1b990d49f35af96be1d5e1d5
Roza13-bit <roizamir876@gmail.com>
2020-12-29 18:17:01 +0200

    V0.6

--- Assets/Scripts/Blend.cs
+++ Assets/Scripts/Blend.cs
@@ -5,14 +5,21 @@ using System.Linq;
 
 public class Blend : MonoBehaviour
 {
+    [Header("Public Cached References")]
     public Transform spawnPoint;
     public Rigidbody rb;
+
+    [Header("Fruit List In Blender")]
     public List<Transform> fruitListBlend = new List<Transform>();
 
-
+    [Header("Shake Spawn Variables")]
+    public int numOfDrops;
+    public float dropsSpawnOffset;
 
     private void OnMouseDown()
     {
+
+
         Debug.Log(fruitListBlend.Count);
         for (var i = 0; i < fruitListBlend.Count; i++)
         {
