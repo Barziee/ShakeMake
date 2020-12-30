c98470a39c73176ebc14e1fcee3340f1ea5b6e0f
Roza13-bit <roizamir876@gmail.com>
2020-12-30 14:20:56 +0200

    V0.7.1 - Added Animation & 3D Assets.
    V0.7.1 - Added Animation & 3D Assets.
    - Added pouring animation(not complete).
    - Added 3D 'Correct' 'Incorrect' models.
    - Changed build to android.
    - Changed some materials & light settings.

--- Assets/Scripts/Blend.cs
+++ Assets/Scripts/Blend.cs
@@ -10,11 +10,17 @@ public class Blend : MonoBehaviour
     public GameObject blenderTopGO;
     public GameObject shakeParticlesGO;
     public Rigidbody rb;
+    public Animator blenderAnimator;
 
     [Header("Fruit List In Blender")]
     public List<Transform> fruitListBlend = new List<Transform>();
 
     private void OnMouseDown()
+    {
+        StartCoroutine(StartBlenderLoop());
+    }
+
+    private IEnumerator StartBlenderLoop()
     {
         blenderTopGO.SetActive(true);
         shakeParticlesGO.SetActive(true);
@@ -23,10 +29,30 @@ public class Blend : MonoBehaviour
         for (var i = 0; i < fruitListBlend.Count; i++)
         {
             fruitListBlend[i].GetComponent<Rigidbody>().isKinematic = true;
+            fruitListBlend[i].GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
             Debug.Log("Fruit added");
         }
 
         rb.AddTorque(0f, 9000f, 0f, ForceMode.Force);
         Debug.Log("BLENDDDDDD");
+
+        yield return new WaitForSeconds(4f);
+
+        foreach (Transform child in spawnPoint) 
+        {
+            Destroy(child.gameObject);
+
+        }
+
+        rb.AddTorque(0f, 0f, 0f, ForceMode.VelocityChange);
+
+        yield return new WaitForSeconds(0.6f);
+
+        blenderTopGO.SetActive(false);
+
+        blenderAnimator.SetTrigger("Pour");
+
     }
+
 }
+
