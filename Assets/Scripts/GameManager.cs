8dc97786df1ed80a1dd87b832bcaf272cd2d58e4
Bar Ronen <barzie02@gmail.com>
2021-01-20 01:05:59 +0200

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,31 +8,11 @@ public class GameManager : MonoBehaviour
     [Header("Game State Managers")]
     public static int numOfCorrectOrdersDelivered;
     public static int numOfGameOrdersFinished;
-
-    void Start()
-    {
-        
-    }
-
-
+ 
     void Update()
     {
         GameWin();
-
     }
-
-
-
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Application.Quit();");
-    }
-
 
     private void GameWin()
     {
